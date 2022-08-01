5289961417e27631327c0deffb33a0bc9af170bd
exbinario <miguel.4to.1994@gmail.com>
2022-07-31 21:45:20 -0600

    intentado realizar la pagina principal

---
+++ paginas/headers/headerbootstrap.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
