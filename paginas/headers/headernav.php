c7bf7c428633db91a8269a4da94b4a56bd03080b
exbinario <miguel.4to.1994@gmail.com>
2022-08-03 22:00:27 -0600

    en el navbar inicio se le agrego link

--- paginas/headers/headernav.php
+++ paginas/headers/headernav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <a class="navbar-brand" href="#">Inicio</a>
+      <a class="navbar-brand" href="/proyectoChalen/paginas/principal.php">Inicio</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
